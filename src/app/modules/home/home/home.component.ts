c9d502c2e6c8447299f34a251ec174a6c69eeac4
luhouyang <luhouyang@gmail.com>
2024-08-01 23:28:59 +0800

    updated navigation bar, fixed links

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -11,21 +11,22 @@ export class HomeComponent {
     {
       header: 'GDSC-UTP GC',
       content:
-        'Google Classroom started to share workshop materials to all UTP students and other interested people. With hands-on tutorians and full explainations on latest technologies such as TensorFlow, Flutter, Firebase, GitHub and more. The materials are maintained by me and my friends as a side project and initiative to raise technicality level of students',
+        'Google Classroom started to share workshop materials to all UTP students and other interested people. With hands-on tutorians and full explainations on latest technologies such as TensorFlow, Flutter, Firebase, GitHub and more.',
       imagePath: '/public/assets/gdsc_utp_logo.webp',
-      hyperlink: 'https://classroom.google.com/c/NjkzNzI5NzE5NTUy?cjc=tlab4o7',
+      // hyperlink: 'https://classroom.google.com/c/NjkzNzI5NzE5NTUy?cjc=tlab4o7',
+      hyperlink: '/projects/#googleclassroom'
     },
     {
-      header: 'testing string',
+      header: 'Coming Soon!',
       content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Exciting new content is on the way! Stay tuned for updates and be the first to discover what’s coming next. Check back soon for more details and information!',
       imagePath: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
       hyperlink: 'https://github.com/luhouyang',
     },
     {
-      header: 'testing string',
+      header: 'Coming Soon!',
       content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Exciting new content is on the way! Stay tuned for updates and be the first to discover what’s coming next. Check back soon for more details and information!',
       imagePath: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
       hyperlink: 'https://github.com/luhouyang',
     },
