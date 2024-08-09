29ce32883b499998e60395769477aae37a7806b3
luhouyang <luhouyang@gmail.com>
2024-08-09 12:15:39 +0800

    seen NGO added

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -13,22 +13,22 @@ export class HomeComponent {
       content:
         'Google Classroom started to share workshop materials to all UTP students and other interested people. With hands-on tutorians and full explainations on latest technologies such as TensorFlow, Flutter, Firebase, GitHub and more.',
       imagePath: '/public/assets/gdsc_utp_logo.webp',
-      // hyperlink: 'https://classroom.google.com/c/NjkzNzI5NzE5NTUy?cjc=tlab4o7',
-      hyperlink: ['projects', ''],
+      hyperlink: 'https://classroom.google.com/c/NjkzNzI5NzE5NTUy?cjc=tlab4o7',
+      // hyperlink: ['projects', ''],
       fragment: 'googleclassroom',
+    },
+    {
+      header: 'SEEN NGO',
+      content:
+        'I developed a dynamic website for Sabah Entitlement & Equity Now (SEEN), a youth-driven NGO focused on educating and empowering Sabahans about their rights. The site highlights key social, economic, and political issues, providing an accessible platform for the youth of Sabah to stay informed and engaged.',
+      imagePath: '/public/assets/seenredlong.webp',
+      hyperlink: 'https://seen-ngo-website.pages.dev/',
+      fragment: 'seenngo',
     },
     {
       header: 'Coming Soon!',
       content:
-        'Exciting new content is on the way! Stay tuned for updates and be the first to discover what\'s coming next. Check back soon for more details and information!',
-      imagePath: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
-      hyperlink: 'https://github.com/luhouyang',
-      fragment: 'googleclassroom',
-    },
-    {
-      header: 'Coming Soon!',
-      content:
-        'Exciting new content is on the way! Stay tuned for updates and be the first to discover what\'s coming next. Check back soon for more details and information!',
+        "Exciting new content is on the way! Stay tuned for updates and be the first to discover what's coming next. Check back soon for more details and information!",
       imagePath: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
       hyperlink: 'https://github.com/luhouyang',
       fragment: 'googleclassroom',
