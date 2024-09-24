33fd15416e59d5812cd325073d45bebcf1083f5c
luhouyang <luhouyang@gmail.com>
2024-09-25 03:18:56 +0800

    updates

--- src/app/modules/contact/contact/contact.component.ts
+++ src/app/modules/contact/contact/contact.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngAfterViewInit() {
+    // Only run this logic if we're in the browser
+    if (isPlatformBrowser(this.platformId)) {
+      const header = document.getElementById('header');
+      if (header) {
+        header.classList.add('scrolled');
+      }
+    }
+  }
+
+  onSubmit() {
+    if (
+      this.contactData.name &&
+      this.contactData.email &&
+      this.contactData.message
+    ) {
+      console.log('Form Data: ', this.contactData);
+      // Placeholder for form submission logic, e.g., send the data to a server
+      alert('Thank you for reaching out! We will get back to you shortly.');
+    }
+  }
 }
